ca4e953853806b5257f5fa1988791dd839c2dad5
Hugo Burd <hugoburd@gmail.com>
2018-10-05 16:00:42 -0700

    initial commit

---
+++ src/main.c
@@ -0,0 +1,203 @@
+//
+// This file is part of the GNU ARM Eclipse distribution.
+// Copyright (c) 2014 Liviu Ionescu.
+//
+
+// ----------------------------------------------------------------------------
+// School: University of Victoria, Canada.
+// Course: ECE 355 "Microprocessor-Based Systems".
+// This is template code for Part 2 of Introductory Lab.
+//
+// See "system/include/cmsis/stm32f0xx.h" for register/bit definitions.
+// See "system/src/cmsis/vectors_stm32f0xx.c" for handler declarations.
+// ----------------------------------------------------------------------------
+
+#include <stdio.h>
+#include "diag/Trace.h"
+#include "cmsis/cmsis_device.h"
+
+// ----------------------------------------------------------------------------
+//
+// STM32F0 empty sample (trace via $(trace)).
+//
+// Trace support is enabled by adding the TRACE macro definition.
+// By default the trace messages are forwarded to the $(trace) output,
+// but can be rerouted to any device or completely suppressed, by
+// changing the definitions required in system/src/diag/trace_impl.c
+// (currently OS_USE_TRACE_ITM, OS_USE_TRACE_SEMIHOSTING_DEBUG/_STDOUT).
+//
+
+// ----- main() ---------------------------------------------------------------
+
+// Sample pragmas to cope with warnings. Please note the related line at
+// the end of this function, used to pop the compiler diagnostics status.
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+#pragma GCC diagnostic ignored "-Wmissing-declarations"
+#pragma GCC diagnostic ignored "-Wreturn-type"
+
+
+/* Clock prescaler for TIM2 timer: no prescaling */
+#define myTIM2_PRESCALER ((uint16_t)0x0000)
+/* Maximum possible setting for overflow */
+#define myTIM2_PERIOD ((uint32_t)0xFFFFFFFF)
+
+void myGPIOA_Init(void);
+void myTIM2_Init(void);
+void myEXTI_Init(void);
+
+/* GLOBALS */
+
+
+int main(int argc, char* argv[])
+{
+
+	trace_printf("This is Part 2 of Introductory Lab...\n");
+	trace_printf("System clock: %u Hz\n", SystemCoreClock);
+
+	myGPIOA_Init();		/* Initialize I/O port PA */
+	myTIM2_Init();		/* Initialize timer TIM2 */
+	myEXTI_Init();		/* Initialize EXTI */
+
+	while (1)
+	{
+		// Nothing is going on here...
+
+	}
+
+	return 0;
+
+}
+
+
+void myGPIOA_Init()
+{
+	/* Enable clock for GPIOA peripheral */
+	RCC->AHBENR |= RCC_AHBENR_GPIOAEN;
+
+	/* Configure PA1 as input */
+	GPIOA->MODER &= ~(GPIO_MODER_MODER1);
+
+	/* Ensure no pull-up/pull-down for PA1 */
+	GPIOA->PUPDR &= ~(GPIO_PUPDR_PUPDR1);
+}
+
+
+void myTIM2_Init()
+{
+	/* Enable clock for TIM2 peripheral */
+	RCC->APB1ENR |= RCC_APB1ENR_TIM2EN;
+
+	/* Configure TIM2: buffer auto-reload, count up, stop on overflow,
+	 * enable update events, interrupt on overflow only */
+	TIM2->CR1 &= ~(TIM_CR1_DIR);	// clear direction bit
+	TIM2->CR1 |= TIM_CR1_ARPE; 		// set auto-reload
+	TIM2->CR1 |= TIM_CR1_URS;		// set overflow only events
+	TIM2->CR1 |= TIM_CR1_OPM;		// set stop on overflow event
+	TIM2->CR1 &= ~(TIM_CR1_UDIS);	// clear update event disable (i.e. enable events)
+
+	/* Set clock prescaler value */
+	TIM2->PSC = myTIM2_PRESCALER;
+	/* Set auto-reloaded delay */
+	TIM2->ARR = myTIM2_PERIOD;
+
+	/* Update timer registers */
+	TIM2->EGR = ((uint16_t)0x0001);
+
+	/* Assign TIM2 interrupt priority = 0 in NVIC */
+	// Relevant register: NVIC->IP[3], or use NVIC_SetPriority
+	NVIC_SetPriority(TIM2_IRQn, 0);
+
+	/* Enable TIM2 interrupts in NVIC */
+	// Relevant register: NVIC->ISER[0], or use NVIC_EnableIRQ
+	NVIC_EnableIRQ(TIM2_IRQn);
+
+	/* Enable update interrupt generation */
+	TIM2->DIER |= TIM_DIER_UIE;
+}
+
+
+void myEXTI_Init()
+{
+	/* Map EXTI1 line to PA1 */
+	SYSCFG->EXTICR[0] = SYSCFG_EXTICR1_EXTI1_PA;
+
+	/* EXTI1 line interrupts: set rising-edge trigger */
+	EXTI->RTSR |= EXTI_RTSR_TR1;
+
+	/* Unmask interrupts from EXTI1 line */
+	EXTI->IMR |= EXTI_IMR_MR1;
+
+	/* Assign EXTI1 interrupt priority = 0 in NVIC */
+	// Relevant register: NVIC->IP[1], or use NVIC_SetPriority
+	NVIC_SetPriority(EXTI0_1_IRQn, 0);
+
+	/* Enable EXTI1 interrupts in NVIC */
+	// Relevant register: NVIC->ISER[0], or use NVIC_EnableIRQ
+	NVIC_EnableIRQ(EXTI0_1_IRQn);
+
+	TIM2->CNT = 0x00000000;			// clear count
+}
+
+
+/* This handler is declared in system/src/cmsis/vectors_stm32f0xx.c */
+void TIM2_IRQHandler()
+{
+	/* Check if update interrupt flag is indeed set */
+	if ((TIM2->SR & TIM_SR_UIF) != 0)
+	{
+		trace_printf("\n*** Overflow! ***\n");
+
+		/* Clear update interrupt flag */
+		TIM2->SR &= ~(TIM_SR_UIF);
+
+		/* Restart stopped timer */
+		TIM2->CR1 |= TIM_CR1_CEN;
+	}
+}
+
+
+/* This handler is declared in system/src/cmsis/vectors_stm32f0xx.c */
+void EXTI0_1_IRQHandler()
+{
+	/* Check if EXTI1 interrupt pending flag is indeed set */
+	if ((EXTI->PR & EXTI_PR_PR1) != 0)
+	{
+		//
+		// 1. If this is the first edge:
+		//	- Clear count register (TIM2->CNT).
+		//	- Start timer (TIM2->CR1).
+		//    Else (this is the second edge):
+		//	- Stop timer (TIM2->CR1).
+		//	- Read out count register (TIM2->CNT).
+		//	- Calculate signal period and frequency.
+		//	- Print calculated values to the console.
+		//	  NOTE: Function trace_printf does not work
+		//	  with floating-point numbers: you must use
+		//	  "unsigned int" type to print your signal
+		//	  period and frequency.
+		//
+		// 2. Clear EXTI1 interrupt pending flag (EXTI->PR).
+		//
+
+		if(!(TIM2->CR1 & TIM_CR1_CEN))
+		{
+			TIM2->CR1 |= TIM_CR1_CEN;		// start timer
+		}
+		else
+		{
+			TIM2->CR1 &= ~(TIM_CR1_CEN);	// stop timer
+			uint32_t count = TIM2->CNT;		// read timer count
+			uint32_t freq = SystemCoreClock / count;
+			uint32_t period = 1000000000 / freq;
+			trace_printf("Count value: %d, frequency: %d Hz, period: %d ns\n", count, freq, period);
+			TIM2->CNT = 0x00000000;			// clear count
+		}
+		EXTI->PR |= EXTI_PR_PR1;		// clear pending bit
+	}
+}
+
+
+#pragma GCC diagnostic pop
+
+// ----------------------------------------------------------------------------
